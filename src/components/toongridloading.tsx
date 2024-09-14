6048dbbb13d207b9c9b7679fda9b2d8b18f19e85
Eun Oh <bdohhhhh@gmail.com>
2024-09-14 19:02:14 +0900

    [update]: toon grid loading 타입스크립트 마이그레이션

---
+++ src/components/toongridloading.tsx
@@ -0,0 +1,10 @@
+"use client";
+import styles from "@/app/work/toon/page.module.css";
+
+export default function ToonGridLoading() {
+  return (
+    <div className={styles.toongridloadingbox}>
+      {/* <img className={styles.ballimg} src='/assets/toon/background/toon_ani.gif' alt='loader'></img> */}
+    </div>
+  );
+}
