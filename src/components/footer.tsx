cd09352e83cd3bd8877dd4f7b088cffacb5b3cfc
Eun Oh <bdohhhhh@gmail.com>
2024-09-14 20:47:07 +0900

    [update]: footer 타입스크립트 마이그레이션

---
+++ src/components/footer.tsx
@@ -0,0 +1,98 @@
+"use client";
+import styles from "@/app/page.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useRef } from "react";
+
+export default function Footer() {
+  const footRef = useRef<HTMLDivElement>(null);
+  const pathname = usePathname();
+
+  return (
+    <div
+      ref={footRef}
+      className={styles.footer}
+      style={{
+        backgroundColor: pathname !== "/" ? "transparent" : "#eee7d1",
+        justifyContent: "space-between",
+        display:
+          pathname === "/about" ||
+          pathname === "/goods" ||
+          pathname === "/work/collaboration"
+            ? "none"
+            : "flex",
+        color:
+          pathname === "/" || pathname === "/contact" ? "#eee7d1" : "#40403d",
+      }}
+    >
+      <p>©2024.nuthazel</p>
+      <Link href={"https://www.instagram.com/bdn._.toon/"} target="_blanc">
+        <div
+          style={{
+            display: "flex",
+            color:
+              pathname === "/" || pathname === "/contact"
+                ? "#eee7d1"
+                : "#40403d",
+          }}
+        >
+          <div
+            className={styles.cong}
+            style={{
+              borderColor:
+                pathname === "/" || pathname === "/contact"
+                  ? "#eee7d1"
+                  : "#40403d",
+            }}
+          >
+            i
+          </div>
+          <div
+            className={styles.cong}
+            style={{
+              borderColor:
+                pathname === "/" || pathname === "/contact"
+                  ? "#eee7d1"
+                  : "#40403d",
+            }}
+          >
+            n
+          </div>
+          <div
+            className={styles.cong}
+            style={{
+              borderColor:
+                pathname === "/" || pathname === "/contact"
+                  ? "#eee7d1"
+                  : "#40403d",
+            }}
+          >
+            s
+          </div>
+          <div
+            className={styles.cong}
+            style={{
+              borderColor:
+                pathname === "/" || pathname === "/contact"
+                  ? "#eee7d1"
+                  : "#40403d",
+            }}
+          >
+            t
+          </div>
+          <div
+            className={styles.cong}
+            style={{
+              borderColor:
+                pathname === "/" || pathname === "/contact"
+                  ? "#eee7d1"
+                  : "#40403d",
+            }}
+          >
+            a
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
