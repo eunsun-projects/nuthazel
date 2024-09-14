b041447bde0abf5c19ab2ce92c899b209553f5cf
Eun Oh <bdohhhhh@gmail.com>
2024-09-14 16:09:56 +0900

    [contact]: page, layout 타입스크립트 마이그레이션

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,9 @@
-import ContactPage from "@/app/contact/_component/contact"
 import { basicMeta, basicViewport } from "@/app/basicmeta";
+import ContactPage from "@/app/contact/_component/contact";
 
 export const metadata = basicMeta;
 export const viewport = basicViewport;
 
-export default function Contact(){
-    return(
-        <ContactPage/>
-    )
+export default function Contact() {
+  return <ContactPage />;
 }
