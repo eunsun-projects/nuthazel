527a80218f1a167b662e5f1cde4afed4624f65b6
Eun Oh <bdohhhhh@gmail.com>
2024-09-14 16:30:02 +0900

    [update]: goods page, layout 타입스크립트 마이그레이션

---
+++ src/app/goods/layout.tsx
@@ -0,0 +1,30 @@
+import SetScreenSize from "@/components/setscreensize";
+import "../globals.css";
+// import Footer from '@/components/footer'
+
+export default function GoodsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <SetScreenSize />
+      <div
+        style={{
+          height: "100%",
+          minHeight: "calc(var(--vh, 1vh) * 100 - 1px)",
+          overflowY: "auto",
+          overflowX: "hidden",
+          // overflow:"hidden",
+          // backgroundColor:'#eee7d1'
+          // backgroundColor:'#634f3e'
+          backgroundColor: "#916b4d",
+        }}
+      >
+        {children}
+        {/* <Footer/> */}
+      </div>
+    </>
+  );
+}
--- src/app/goods/page.tsx
+++ src/app/goods/page.tsx
@@ -1,11 +1,9 @@
-import Goods from "./_component/goods"
 import { basicMeta, basicViewport } from "@/app/basicmeta";
+import Goods from "./_component/goods";
 
 export const metadata = basicMeta;
 export const viewport = basicViewport;
 
-export default function GoodsPage(){
-    return(
-        <Goods/>
-    )
+export default function GoodsPage() {
+  return <Goods />;
 }
