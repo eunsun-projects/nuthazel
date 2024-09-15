98d77c1a2496ca3ae0abb75de5003c30932b5178
Eun Oh <bdohhhhh@gmail.com>
2024-09-15 16:29:19 +0900

    [update]: nextauth route.ts 에서도 token 올바르게 처리

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,26 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
 import { FirestoreAdapter } from "@next-auth/firebase-adapter";
-import { cert, ServiceAccount } from "firebase-admin/app"
-import * as token from '../../../../../nuthazel-firebase-key.json'
+import { cert } from "firebase-admin/app";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
-    // Configure one or more authentication providers
-    adapter: FirestoreAdapter({
-        credential: cert(token as ServiceAccount)
+  // Configure one or more authentication providers
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
-	providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "", 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-        }),
-        // ...add more providers here
-    ],
-    secret : process.env.NEXTAUTH_SECRET,
-}
+  }),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST }
-
+export { handler as GET, handler as POST };
