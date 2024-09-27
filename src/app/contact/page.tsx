f698cfe8705169f0a3cacd8447326ff7b9fb7d2d
Eun Oh <bdohhhhh@gmail.com>
2024-09-27 22:39:47 +0900

    [del]

--- src/app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import { basicMeta, basicViewport } from "@/app/basicmeta";
-import ContactPage from "@/app/contact/_component/contact";
-
-export const metadata = basicMeta;
-export const viewport = basicViewport;
-
-export default function Contact() {
-  return <ContactPage />;
-}
--- src/app/goods/page.tsx
+++
@@ -1,9 +0,0 @@
-import { basicMeta, basicViewport } from "@/app/basicmeta";
-import Goods from "./_component/goods";
-
-export const metadata = basicMeta;
-export const viewport = basicViewport;
-
-export default function GoodsPage() {
-  return <Goods />;
-}
