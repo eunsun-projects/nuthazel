0f9547f95ee8b724e4a52fdbe7cbf87c9b2a577a
Eun Oh <bdohhhhh@gmail.com>
2024-09-28 15:08:03 +0900

    [update]: query index

--- src/hooks/queries/index.ts
+++ src/hooks/queries/index.ts
@@ -1,4 +1,5 @@
 export { useCollaboQuery } from "./collabo.query";
 export { useIllustQuery } from "./illust.query";
 export { useLogInQuery } from "./login.query";
+export { useToonQuery } from "./toon.query";
 export { useUserQuery } from "./users.query";
